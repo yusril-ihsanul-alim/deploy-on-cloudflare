57757331c13dc6add085bc9b925cca56a2220259
yusril-Ihsanul-alim <yusril.ihsanul.alim@gmail.com>
2022-11-21 01:50:46 +0700

    update fix

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
